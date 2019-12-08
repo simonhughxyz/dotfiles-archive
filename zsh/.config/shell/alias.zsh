99419b33eeaea1e09a2a92a72d0793b10d9ebdd8
simonhughcom <simonhughdev@gmail.com>
2019-12-08 05:41:02 +0000

    Go back in directory history using number 1-5.

--- zsh/.config/shell/alias.zsh
+++ zsh/.config/shell/alias.zsh
@@ -3,6 +3,13 @@
 alias -s mp4=mpv
 alias -s mkv=mpv
 
+#cd
+alias 1='cd -'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+
 alias -g G='| rg -i'
 alias -g C='| wc -l'
 alias -g L='| less'
