c37af68785f9c0c9b6394a32c4fa3c82d3510cff
simonhughcom <simonhughdev@gmail.com>
2019-12-10 16:40:46 +0000

    Added more suffix aliases for jpg,png,pdf etc.

--- zsh/.config/shell/alias.zsh
+++ zsh/.config/shell/alias.zsh
@@ -1,15 +1,23 @@
 # ZSH alias
 
+# Suffix
 alias -s mp4=mpv
 alias -s mkv=mpv
+alias -s mp3=mpv
+alias -s jpg=sxiv
+alias -s png=sxiv
+alias -s pdf=zathura
+alias -s ps=zathura
 
-#cd
+
+# cd
 alias 1='cd -'
 alias 2='cd -2'
 alias 3='cd -3'
 alias 4='cd -4'
 alias 5='cd -5'
 
+# Global
 alias -g G='| rg -i'
 alias -g C='| wc -l'
 alias -g L='| less'
