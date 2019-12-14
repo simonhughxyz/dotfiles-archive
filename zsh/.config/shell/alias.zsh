f6908c3e5474693cc653b5c71ec337dc6bfa4abb
simonhughcom <simonhughdev@gmail.com>
2019-12-14 03:16:46 +0000

    Grouped suffix aliases together.

--- zsh/.config/shell/alias.zsh
+++ zsh/.config/shell/alias.zsh
@@ -1,15 +1,19 @@
 # ZSH alias
 
-alias -s mp4=mpv
-alias -s mkv=mpv
+# Suffix
+alias -s {mp4,MP4,mkv,MKV,mp3,MP3,mov,MOV}='mpv'
+alias -s {jpg,JPG,png,PNG}='sxiv'
+alias -s {pdf,PDF,ps,PS}='zathura'
 
-#cd
+
+# cd
 alias 1='cd -'
 alias 2='cd -2'
 alias 3='cd -3'
 alias 4='cd -4'
 alias 5='cd -5'
 
+# Global
 alias -g G='| rg -i'
 alias -g C='| wc -l'
 alias -g L='| less'
