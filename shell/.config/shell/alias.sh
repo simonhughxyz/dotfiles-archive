df4a547d91fc632a65fac7d4e5a82739ba5c9da2
Simon H Moore <simonhughdev@gmail.com>
2020-01-25 05:26:46 +0000

    Aliases for iptables.
    Use ip{6}t for ip{6}tables.
    Use ip{6}tl for listing ip{6}table rules in less.

--- shell/.config/shell/alias.sh
+++ shell/.config/shell/alias.sh
@@ -117,6 +117,10 @@ alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^
 # networking
 alias ipinfo='curl ifconfig.me/all && echo ""'
 alias myip='curl ifconfig.me/ip; echo'
+alias ipt='iptables'
+alias ip6t='ip6tables'
+alias iptl='iptables -nvL | less'
+alias ip6tl='ip6tables -nvL | less'
 
 # transmission
 alias tms='transmission-daemon'
