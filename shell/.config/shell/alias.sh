5e0290bf90a9a268d151f88ce833e2d2e68d4143
Simon H Moore <simonhughdev@gmail.com>
2020-02-04 23:29:33 +0000

    Color manpage.

--- shell/.config/shell/alias.sh
+++ shell/.config/shell/alias.sh
@@ -142,3 +142,16 @@ alias mkdir='mkdir -pv'     # Add parent dir on demand
 alias qcp='qcp -f do'   # Destination column only.
 alias qmv='qmv -f do'   # Destination column only.
 
+# functions
+
+# Color for manpages
+man() {
+  env LESS_TERMCAP_mb=$'\E[01;31m' \
+  LESS_TERMCAP_md=$'\E[01;31m' \
+  LESS_TERMCAP_me=$'\E[0m' \
+  LESS_TERMCAP_se=$'\E[0m' \
+  LESS_TERMCAP_so=$'\E[01;44;33m' \
+  LESS_TERMCAP_ue=$'\E[0m' \
+  LESS_TERMCAP_us=$'\E[01;32m' \
+  man "$@"
+}
