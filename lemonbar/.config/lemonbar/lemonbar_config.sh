3958b23c41ec6894b7f8b0febf90accf298decd7
Simon H Moore <simonhughdev@gmail.com>
2020-02-18 15:36:53 +0000

    First commit.

---
+++ lemonbar/.config/lemonbar/lemonbar_config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# LEMONBAR_CONFIG
+# Simon Hugh Moore
+#
+# Set config options for lemonbar
+
+# font="-xos4-terminesspowerline-medium-r-normal--12-120-72-72-c-60-iso10646-1"
+font="-xos4-terminus-bold-r-normal--28-280-72-72-c-140-iso10646-1"
+icon_font="-xos4-terminusicons2mono-medium-r-normal--12-120-72-72-m-60-iso8859-1"
+geometry="x50"
+f_color="#DDDDDD"
+# b_color="#aaaaaa"
+
+# sh ./statusline.sh | lemonbar -p -f "${font}" -f "${icon_font}" -g "${geometry}" -B "${b_color}" -F "${f_color}"
+sh ./statusline.sh | lemonbar -p -f "${font}" -f "${icon_font}" -g "${geometry}" -F "${f_color}"
