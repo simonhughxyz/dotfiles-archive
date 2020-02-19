46dc18b0d80c5f851c57b569226ee6a78d72adab
Simon H Moore <simonhughdev@gmail.com>
2020-02-19 10:58:10 +0000

    Background color set to '#202020'.

--- lemonbar/.config/lemonbar/lemonbar_config.sh
+++ lemonbar/.config/lemonbar/lemonbar_config.sh
@@ -10,7 +10,7 @@ font="-xos4-terminus-bold-r-normal--28-280-72-72-c-140-iso10646-1"
 icon_font="-xos4-terminusicons2mono-medium-r-normal--12-120-72-72-m-60-iso8859-1"
 geometry="x30"
 f_color="#DDDDDD"
-# b_color="#aaaaaa"
+b_color="#202020"
 
 # sh ./statusline.sh | lemonbar -p -f "${font}" -f "${icon_font}" -g "${geometry}" -B "${b_color}" -F "${f_color}"
 sh ./statusline.sh | lemonbar -p -f "${font}" -f "${icon_font}" -g "${geometry}" -F "${f_color}"
