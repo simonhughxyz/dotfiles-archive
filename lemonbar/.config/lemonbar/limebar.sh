2b9b5d08e2aac2006226aa9e147c9d78392326ec
Simon H Moore <simonhughdev@gmail.com>
2020-02-21 00:27:53 +0000

    Kill any running lemonbar.

--- lemonbar/.config/lemonbar/limebar.sh
+++ lemonbar/.config/lemonbar/limebar.sh
@@ -5,6 +5,9 @@
 #
 # Set config options for lemonbar
 
+# Kill any running lemonbar
+pgrep -x lemonbar > /dev/null && pkill -x lemonbar &
+
 # font="-xos4-terminesspowerline-medium-r-normal--12-120-72-72-c-60-iso10646-1"
 font="-xos4-terminus-bold-r-normal--28-280-72-72-c-140-iso10646-1"
 # icon_font="-xos4-terminusicons2mono-medium-r-normal--12-120-72-72-m-60-iso8859-1"
