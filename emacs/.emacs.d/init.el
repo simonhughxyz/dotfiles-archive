20e91d8b07ea6cdd092349897e0550ae6990c720
Simon H Moore <simonhughdev@gmail.com>
2020-04-25 04:15:32 +0100

    Removed unnecessary code.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,18 +6,6 @@
  
 (setq package-enable-at-startup nil)
 (package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil use-package evil-visual-mark-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
