03c5c21aa0b313a9460a31aa4c92a5845ae5c781
Simon H Moore <simonhughdev@gmail.com>
2020-05-02 06:39:35 +0100

    OSC is visible by default.

--- mpv/.config/mpv/scripts/osc-toggle.lua
+++ mpv/.config/mpv/scripts/osc-toggle.lua
@@ -3,7 +3,11 @@
 -- 
 -- Expects osc to be set to never by default.
 
-local is_never = true  -- no way to read the osc mode, so we assume it starts in never mode.
+
+-- OSC visibility set to always by default.
+mp.command("script-message osc-visibility always")
+
+local is_never = false  -- no way to read the osc mode, so we assume it starts in never mode.
 mp.add_key_binding("b", "osc-toggle", function()
     mp.command("script-message osc-visibility " .. (is_never and "always" or "never"))
     is_never = not is_never
