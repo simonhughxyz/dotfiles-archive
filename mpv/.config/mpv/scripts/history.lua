fec9d2b5d23457ec7fa5a1e3548d807384a70ab1
Simon H Moore <simonhughdev@gmail.com>
2020-05-04 06:39:53 +0100

    Fix: Don't show full path for streams.

--- mpv/.config/mpv/scripts/history.lua
+++ mpv/.config/mpv/scripts/history.lua
@@ -11,10 +11,15 @@ local HISTFILE = (os.getenv('APPDATA') or os.getenv('HOME')..'/.local/share')..'
 mp.register_event('file-loaded', function()
     local title, path, logfile;
 
+    -- get title or use '---'.
     title = mp.get_property('media-title');  
     title = (title == mp.get_property('filename') and '---' or title);
     
-    path = utils.join_path(mp.get_property("working-directory"), mp.get_property('path'))
+    -- get full path if local file.
+    path = mp.get_property('path')
+    if not path:find("http.?://") or path:find("magnet:%?") then
+        path = utils.join_path(mp.get_property("working-directory"), path)
+    end
 
     logfile = io.open(HISTFILE, 'a+');
     logfile:write(('%s   %s   %s\n'):format(os.date('%Y-%m-%d   %H:%M:%S'), path, title));    
