c3358ef24389b1f00adf6e97f4f71a38bcb338ed
Simon H Moore <simonhughdev@gmail.com>
2020-05-04 06:30:29 +0100

    Fix: Now using full path for local files.

--- mpv/.config/mpv/scripts/history.lua
+++ mpv/.config/mpv/scripts/history.lua
@@ -4,15 +4,19 @@
 -- Format: "Date Time Path Title" each seperated by 3 spaces.
 -- Lack of title is indicated by '---'.
 
+local utils = require("mp.utils")
+
 local HISTFILE = (os.getenv('APPDATA') or os.getenv('HOME')..'/.local/share')..'/mpv/history.log';
 
 mp.register_event('file-loaded', function()
-    local title, logfile;
+    local title, path, logfile;
 
     title = mp.get_property('media-title');  
     title = (title == mp.get_property('filename') and '---' or title);
+    
+    path = utils.join_path(mp.get_property("working-directory"), mp.get_property('path'))
 
     logfile = io.open(HISTFILE, 'a+');
-    logfile:write(('%s   %s   %s\n'):format(os.date('%Y-%m-%d   %H:%M:%S'), mp.get_property('path'), title));    
+    logfile:write(('%s   %s   %s\n'):format(os.date('%Y-%m-%d   %H:%M:%S'), path, title));    
     logfile:close();
 end)
