b1aae7b2c355d8e594e42ec5d9023feb3fc8e046
Simon H Moore <simonhughdev@gmail.com>
2020-05-23 03:33:55 +0100

    Shortcuts to easily get login or url.

---
+++ pass/.password-store/.extensions/login.bash
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# LOGIN
+# Simon Hugh Moore
+#
+# Retrieve login from pass file.
+# Requires the get extensions to work.
+
+pass get "login" "$@"
---
+++ pass/.password-store/.extensions/url.bash
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# LOGIN
+# Simon Hugh Moore
+#
+# Retrieve url from pass file
+# Required the get extension to work.
+
+pass get "URL" "$@"
