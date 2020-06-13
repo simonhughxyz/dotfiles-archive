3ee9036f4b950be9cc77c3d9707c78d4fcc894f7
Simon H Moore <simonhughdev@gmail.com>
2020-06-13 04:11:18 +0100

    Pass: export now exports to current directory.

--- pass/.password-store/.extensions/export.bash
+++ pass/.password-store/.extensions/export.bash
@@ -6,23 +6,21 @@
 # Export password store as encrypted or plain tar package.
 # If encrypted, it uses the same gpg key as password-store.
 
-prefix=~/
-password_store=~/.password-store    # location of password-store
-gpg_id="$(cat "$password_store/.gpg-id")"       # get gpg-id from password-store
-now="$(date '+%Y-%m-%d_%H-%M-%S')"
+password_store=~/.password-store          # location of password-store
+gpg_id="$(cat "$password_store/.gpg-id")" # get gpg-id from password-store
+now="$(date '+%Y-%m-%d_%H-%M-%S')"        # current date and time.
 
 _plain() {
     tar -zcf - -C "$password_store" ./ 
 }
 
 _encrypt() {
-    plain | gpg -e -r "$gpg_id"
+    _plain | gpg2 -e -r "$gpg_id"
 }
 
 
 case "$1" in
-    -p|--plain) shift; _plain > "$prefix/pass.$now.tar.gz" ;;
-    -e|--encrypt) _encrypt > "$prefix/pass.$now.tar.gz.gpg" ;;
-    *) _encrypt > "$prefix/pass.$now.tar.gz.gpg" ;;
+    -p|--plain) shift; _plain > "pass.$now.tar.gz" ;;
+    -e|--encrypt) shift; _encrypt > "pass.$now.tar.gz.gpg" ;;
+    *) _encrypt > "pass.$now.tar.gz.gpg" ;;
 esac
-exit 0
