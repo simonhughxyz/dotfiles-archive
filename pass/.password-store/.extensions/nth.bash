2f19b64a623cfe17761538c6422e4d172a70d94e
Simon H Moore <simon@simonhugh.xyz>
2020-10-10 03:57:58 +0100

    Pass: Add argument 'get' to 'nth' other values

--- pass/.password-store/.extensions/nth.bash
+++ pass/.password-store/.extensions/nth.bash
@@ -10,10 +10,16 @@
 # get last argument (pass file name).
 for pass_file in "$@";do :;done
 
-# get the password
-_pass="$( pass show "$pass_file" | head -n1 )"
+if [ "$1" == "get" ]; then
+    _value="$( pass get "$2" "$pass_file" )"
+    shift 2
+else
+    # get the password
+    _value="$( pass show "$pass_file" | head -n1 )"
+fi
+
 
 # print the desired character.
 for arg in "$@"; do
-    [ "$arg" != "$pass_file" ] && echo "$_pass" | cut -c "$arg"
+    [ "$arg" != "$pass_file" ] && echo "$_value" | cut -c "$arg"
 done
