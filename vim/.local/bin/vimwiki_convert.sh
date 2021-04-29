af2b873cb9f46449388c7a8e3988a17710df74df
Simon H Moore <simon@simonhugh.xyz>
2021-04-30 00:01:48 +0100

    Vim: Add descriptive comments for arguments

--- vim/.local/bin/vimwiki_convert.sh
+++ vim/.local/bin/vimwiki_convert.sh
@@ -8,14 +8,14 @@
 # Desc: Convert markdown files from vimwiki
 
 # Input arguments passed from vimwiki (see `:h vimwiki-option-custom_wiki2html`)
-force="${1}"
-syntax="${2}"
-extension="${3}"
-output_dir="${4}"
-input_file="${5}"
-css_file="${6}"
-template_path="${7}"
-template_default="${8}"
-template_ext="${9}"
-root_path="${10}"
-custom_args="${11}"
+force="${1}"            # [0/1] overwrite an existing file
+syntax="${2}"           # the syntax chosen for this wiki
+extension="${3}"        # the file extension for this wiki
+output_dir="${4}"       # the full path of the output directory
+input_file="${5}"       # the full path of the wiki page
+css_file="${6}"         # the full path of the css file for this wiki
+template_path="${7}"    # the full path to the wiki's templates
+template_default="${8}" # the default template name
+template_ext="${9}"     # the extension of template files
+root_path="${10}"       # a count of ../ for pages buried in subdirs
+custom_args="${11}"     # custom arguments that will be passed to the conversion
