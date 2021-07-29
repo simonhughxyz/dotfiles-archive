7c065edb0df04e7d50b8c706454953660d679ab3
Simon H Moore <simon@simonhugh.xyz>
2021-07-29 21:50:52 +0100

    add [vim]: require lua modules for init

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,6 +1,10 @@
 -- init.lua
 
-require('lua/settings')    -- lua/settings.lua
+require('settings')    -- lua/settings.lua
+require('plugins')    -- lua/settings.lua
+require('mappings')    -- lua/mappings.lua
+require('highlights')   -- lua/highlights.lua
+require('statusline') -- lua/statusline.lua
 
 -- source my old vimscript config
 vim.cmd('source old_config.vim')
