0ad44b50ef86a8b107ca46176ebd46175307986d
Simon H Moore <simon@simonhugh.xyz>
2021-07-29 21:51:25 +0100

    refactor [vim]: remove source old vim in init

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -5,6 +5,3 @@ require('plugins')    -- lua/settings.lua
 require('mappings')    -- lua/mappings.lua
 require('highlights')   -- lua/highlights.lua
 require('statusline') -- lua/statusline.lua
-
--- source my old vimscript config
-vim.cmd('source old_config.vim')
