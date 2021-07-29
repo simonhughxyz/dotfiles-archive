89ad495d042e287e814e18d1c62ec533866a8afc
Simon H Moore <simon@simonhugh.xyz>
2021-07-29 21:48:21 +0100

    add [vim]: nvim-colorizer config

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -142,7 +142,23 @@ return require('packer').startup(function()
     --     branch = 'next',
     --     run =  'bash install.sh'
     -- }
-    use 'norcalli/nvim-colorizer.lua'
+    use {
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            require 'colorizer'.setup({'*';},
+                {
+                   RGB      = true;
+                   RRGGBB   = true;
+                   names    = true;
+                   RRGGBBAA = true;
+                   rgb_fn   = true;
+                   hsl_fn   = true;
+                   css      = true;
+                   css_fn   = true;
+                   mode     = 'background';
+                })
+        end
+    }
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
