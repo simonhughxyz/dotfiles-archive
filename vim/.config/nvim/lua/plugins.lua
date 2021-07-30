ff52911c2aa6e5fe87ce2bc356956da7a259c226
Simon H Moore <simon@simonhugh.xyz>
2021-07-31 00:42:21 +0100

    add [vim]: treesitter plugin config

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -205,7 +205,44 @@ return require('packer').startup(function()
     }
     use {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
+        branch = '0.5-compat',
+        run = ':TSUpdate',
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                highlight = {
+                    enable = true,
+                    disable = {},
+                },
+                indent = {
+                    enable = true,
+                    disable = {},
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
+                },
+                ensure_installed = {
+                    'lua',
+                    'regex',
+                    'bash',
+                    'c',
+                    'html',
+                    'css',
+                    'javascript',
+                    'typescript',
+                    'go',
+                    'python',
+                    'json',
+                    'toml',
+                    'yaml',
+                },
+            }
+        end
     }
     
 
