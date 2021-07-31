a6995b407c6b006113d7045017a94e33fb782cfc
Simon H Moore <simon@simonhugh.xyz>
2021-07-31 04:25:19 +0100

    add [vim]: telescope.nvim plugin

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -37,6 +37,67 @@ return require('packer').startup(function()
     --     run = './autogen.sh;./configure --prefix=$HOME/.local;make install'
     -- }
     use {
+        'nvim-telescope/telescope.nvim',
+        requires = {
+            {'nvim-lua/popup.nvim'},
+            {'nvim-lua/plenary.nvim'},
+        },
+        cmd = 'Telescope',
+        config = function()
+            require('telescope').setup{
+                defaults = {
+                    vimgrep_arguments = {
+                        'rg',
+                        '--color=never',
+                        '--no-heading',
+                        '--with-filename',
+                        '--line-number',
+                        '--column',
+                        '--smart-case'
+                    },
+                    prompt_prefix = "> ",
+                    selection_caret = "> ",
+                    entry_prefix = "  ",
+                    initial_mode = "insert",
+                    selection_strategy = "reset",
+                    sorting_strategy = "descending",
+                    layout_strategy = "horizontal",
+                    layout_config = {
+                        horizontal = {
+                            mirror = false,
+                            prompt = bottom,
+                            preview_width = 0.50,
+                            preview_cutoff = 80,
+                        },
+                        vertical = {
+                            mirror = false,
+                            prompt = bottom,
+                            preview_height = 0.40,
+                            preview_cutoff = 80,
+                        },
+                        width = 0.96,
+                        height = 0.80,
+                    },
+                    file_sorter =  require('telescope.sorters').get_fuzzy_file,
+                    file_ignore_patterns = {},
+                    generic_sorter =  require('telescope.sorters').get_generic_fuzzy_sorter,
+                    winblend = 0,
+                    border = {},
+                    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+                    color_devicons = true,
+                    use_less = true,
+                    path_display = {},
+                    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+                    previewer = true,
+                    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+                    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+                    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+                    buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker
+                }
+            }
+        end
+    }
+    use {
         'junegunn/fzf',
         dir = '~/.fzf',
         run = './install --all',
