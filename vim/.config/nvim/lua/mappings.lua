a4b3fdc21c611614028105f1221506ecfea08401
Simon H Moore <simon@simonhugh.xyz>
2021-08-06 01:46:36 +0100

    refactor [vim]: move map functions to utils module

--- vim/.config/nvim/lua/mappings.lua
+++ vim/.config/nvim/lua/mappings.lua
@@ -1,18 +1,8 @@
 -- mappings.lua
 
--- need a map method to handle the different kinds of key maps
-local function map(mode, combo, mapping, opts)
-    local options = {noremap = true}
-    if opts then
-        options = vim.tbl_extend('force', options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, combo, mapping, options)
-end
-
--- leader map method
-local function lmap(mode, combo, mapping, opts)
-    map(mode, '<leader>'..combo, mapping, opts)    
-end
+utils = require('utils')
+map = utils.map
+lmap = utils.lmap
 
 -- leader key
 vim.g.mapleader = ' '
