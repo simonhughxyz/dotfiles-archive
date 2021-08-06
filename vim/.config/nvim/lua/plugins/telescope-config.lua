f2fdd0d54bad94206b90e6978bc7eab9dbdc326b
Simon H Moore <simon@simonhugh.xyz>
2021-08-06 01:48:17 +0100

    refactor [vim]: move telescope config to its own module

---
+++ vim/.config/nvim/lua/plugins/telescope-config.lua
@@ -0,0 +1,53 @@
+-- telescope.lua
+
+require('telescope').setup{
+    defaults = {
+    vimgrep_arguments = {
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case'
+    },
+    prompt_prefix = "> ",
+    selection_caret = "> ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_config = {
+        horizontal = {
+            mirror = false,
+            prompt = bottom,
+            preview_width = 0.50,
+            preview_cutoff = 80,
+        },
+        vertical = {
+            mirror = false,
+            prompt = bottom,
+            preview_height = 0.40,
+            preview_cutoff = 80,
+        },
+        width = 0.96,
+        height = 0.80,
+    },
+    file_sorter =  require('telescope.sorters').get_fuzzy_file,
+    file_ignore_patterns = {},
+    generic_sorter =  require('telescope.sorters').get_generic_fuzzy_sorter,
+    winblend = 0,
+    border = {},
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    color_devicons = true,
+    use_less = true,
+    path_display = {},
+    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+    previewer = true,
+    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
+    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
+    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+    buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker
+}
+}
