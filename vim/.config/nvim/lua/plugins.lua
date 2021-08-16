bd9b286c9e49f9091f294ca8632c83a3df949ec9
Simon H Moore <simon@simonhugh.xyz>
2021-08-16 17:32:16 +0100

    refactor[vim]: move vimwiki config to its own module

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -180,26 +180,7 @@ return require('packer').startup(function()
     }
     use {
         'vimwiki/vimwiki',
-        setup = function()
-            vim.g.vimwiki_auto_header = 1
-            vim.g.vimwiki_folding = 'custom'
-            vim.g.vimwiki_key_mappings = { links = 0 }
-            vim.g.vimwiki_list = {
-                {
-                    path = '~/Documents/notes/',
-                    path_html = '~/Documents/notes/build/',
-                    custom_wiki2html = '~/.local/bin/vimwiki_convert.sh',
-                    syntax = 'markdown',
-                    ext = '.md',
-                    links_space_char = '_'
-                }
-            }
-            vim.g.vimwiki_ext2syntax = {
-                ['.md'] = 'markdown',
-                ['.markdown'] = 'markdown',
-                ['.mdown'] = 'markdown'
-            }
-        end
+        setup = function() require('plugins.vimwiki-config') end
     }
     -- use {
     --     'autozimu/LanguageClient-neovim',
