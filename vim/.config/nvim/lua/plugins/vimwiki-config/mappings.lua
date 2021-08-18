4939c262f6a3159cdd1780204f2435d85e6acbe8
Simon H Moore <simon@simonhugh.xyz>
2021-08-18 01:28:43 +0100

    fix[vim]: vimwiki follow link and back link

--- vim/.config/nvim/lua/plugins/vimwiki-config/mappings.lua
+++ vim/.config/nvim/lua/plugins/vimwiki-config/mappings.lua
@@ -5,5 +5,7 @@ map = require('utils').map
 
 silent = {silent = true}
 
+map('n', '<CR>', ':VimwikiFollowLink<CR>', silent)
+map('n', '<Backspace>', ':VimwikiGoBackLink<CR>', silent)
 map('n', '<Tab>', ':VimwikiNextLink<CR>zMzv', silent)
 map('n', '<S-Tab>', ':VimwikiPrevLink<CR>zMzv', silent)
