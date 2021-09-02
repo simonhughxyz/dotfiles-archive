9804ea5965437e0a4eacc8bfac15ea8137dbc8ef
Simon H Moore <simon@simonhugh.xyz>
2021-09-02 23:21:24 +0100

    refactor[vim]: move treesitter-textobjects config to its own module

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -31,63 +31,7 @@ return require('packer').startup(function()
         'nvim-treesitter/nvim-treesitter-textobjects',
         branch = '0.5-compat',
         requires = {'nvim-treesitter/nvim-treesitter'},
-        config = function()
-            require'nvim-treesitter.configs'.setup {
-              textobjects = {
-                select = {
-                  enable = true,
-                  -- Automatically jump forward to textobj, similar to targets.vim 
-                  lookahead = true,
-                  keymaps = {
-                    -- You can use the capture groups defined in textobjects.scm
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                    ["aa"] = "@parameter.outer",
-                    ["ia"] = "@parameter.inner",
-                    ["al"] = "@loop.outer",
-                    ["il"] = "@loop.inner",
-                    ["a#"] = "@comment.outer",
-                    ["i#"] = "@comment.outer",
-                },
-                },
-                 swap = {
-                    enable = true,
-                    swap_next = {
-                        ["<leader>a"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        ["<leader>A"] = "@parameter.inner",
-                    },
-                },
-            move = {
-                  enable = true,
-                  set_jumps = true, -- whether to set jumps in the jumplist
-                  goto_next_start = {
-                    ["]f"] = "@function.outer",
-                    ["]c"] = "@class.outer",
-                    ["]#"] = "@comment.outer",
-                  },
-                  goto_next_end = {
-                    ["]F"] = "@function.outer",
-                    ["]C"] = "@class.outer",
-                    ["]~"] = "@comment.outer",
-                  },
-                  goto_previous_start = {
-                    ["[f"] = "@function.outer",
-                    ["[c"] = "@class.outer",
-                    ["[#"] = "@comment.outer",
-                  },
-                  goto_previous_end = {
-                    ["[F"] = "@function.outer",
-                    ["[C"] = "@class.outer",
-                    ["[~"] = "@comment.outer",
-                  },
-                },
-              },
-            }
-        end
+        config = function() require('plugins.treesitter-textobjects-config') end
     }
 
     -- use 'luochen1990/rainbow' -- Color matching brackets.
