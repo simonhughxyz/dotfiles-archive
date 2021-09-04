7d5b3872ecc11f1adcedc9b1cbea6fd3d5ff1e1a
Simon H Moore <simon@simonhugh.xyz>
2021-09-04 01:52:49 +0100

    refactor[vim]: move nvim-colorizer config to its own module

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -137,20 +137,7 @@ return require('packer').startup(function()
     -- }
     use {
         'norcalli/nvim-colorizer.lua',
-        config = function()
-            require 'colorizer'.setup({'*';},
-                {
-                   RGB      = true;
-                   RRGGBB   = true;
-                   names    = true;
-                   RRGGBBAA = true;
-                   rgb_fn   = true;
-                   hsl_fn   = true;
-                   css      = true;
-                   css_fn   = true;
-                   mode     = 'background';
-                })
-        end
+        config = function() require('plugins.colorizer-config') end
     }
     
 
