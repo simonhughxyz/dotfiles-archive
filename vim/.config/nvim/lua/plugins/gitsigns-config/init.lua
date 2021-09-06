f146c7327d8ad736bf0fe5effd3c8f9d05c3e925
Simon H Moore <simon@simonhugh.xyz>
2021-09-06 01:09:47 +0100

    refactor[vim]: remove and change depreciated gitsigns options

--- vim/.config/nvim/lua/plugins/gitsigns-config/init.lua
+++ vim/.config/nvim/lua/plugins/gitsigns-config/init.lua
@@ -37,12 +37,13 @@ require('gitsigns').setup {
         follow_files = true
     },
     current_line_blame = false,
-    current_line_blame_delay = 1000,
-    current_line_blame_position = 'eol',
+    current_line_blame_opts = {
+        delay = 1000,
+        virt_text_pos = 'eol',
+    },
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
     word_diff = false,
-    use_decoration_api = true,
     use_internal_diff = true,  -- If luajit is present
 }
