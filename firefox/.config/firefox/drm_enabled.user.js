0ec3d194f6a5d66e4ad79f01d549267a348afe1b
Simon H Moore <simon@simonhugh.xyz>
2021-09-07 13:57:34 +0100

    fix[firefox]: enable widevine in drm_enables.user.js

--- firefox/.config/firefox/drm_enabled.user.js
+++ firefox/.config/firefox/drm_enabled.user.js
@@ -5,3 +5,7 @@
 
 // if enabled, automatically downloads the Widevine Content Decryption Module provided by Google Inc.
 user_pref("media.eme.enabled", true);
+
+// Enable the Widevine Content Decryption Module provided by Google Inc.,
+// used for the playback of DRM-controlled HTML5 content.
+user_pref("media.gmp-widevinecdm.enabled", true);
