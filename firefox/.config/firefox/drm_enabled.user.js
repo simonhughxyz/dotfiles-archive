f51c55b5afd764e924e9864372788fb463424f62
Simon H Moore <simon@simonhugh.xyz>
2021-09-07 12:48:59 +0100

    add[firefox]: drm_enabled.user.js

---
+++ firefox/.config/firefox/drm_enabled.user.js
@@ -0,0 +1,7 @@
+//
+// #### DRM_ENABLED.USER.JS ####
+//
+// Enables playback of DRM-controlled HTML5 content
+
+// if enabled, automatically downloads the Widevine Content Decryption Module provided by Google Inc.
+user_pref("media.eme.enabled", true);
