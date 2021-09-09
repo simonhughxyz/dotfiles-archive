1b15869b6c18b110ad5551cc97f3a683998b1a36
Simon H Moore <simon@simonhugh.xyz>
2021-09-09 03:26:07 +0100

    add[ytfzf]: reload job for ts menu

--- ytfzf/.config/ytfzf/conf.sh
+++ ytfzf/.config/ytfzf/conf.sh
@@ -115,7 +115,11 @@ ts_menu () {
     ts -l | sed '1d;s/]sh -c .*$//;s/[[:space:]].*\[/\t/' | \
             fzf -d'\t' --with-nth='2..' --preview='ts -c {1} | grep --line-buffered "^ERROR:\|^\[download\]\|\[youtube\]"' \
             --preview-window='bottom,1%,nowrap,follow' \
-            --bind='alt-q:execute(ts -k {1})'
+            --bind='alt-q:execute(ts -k {1}),alt-r:execute(ts -k {};
+                cmd="$( ts -i {1} | grep "^Command: " | sed "s/^Command: //" )";
+                id="$( echo {1} | sed "s/'"'"'//" )";
+                lable="$( ts -l | grep "^$id" | grep -o "\[.*\]sh -c" | sed "s/^\[//;s/\]sh -c//" )";
+                ts -L "$lable" "$cmd";)'
 }
 
 # Command to interact with task spooler
