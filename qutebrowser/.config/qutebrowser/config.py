4680d6d2a075f9a41bfa862cb350dba5b5c5aa7b
Simon H Moore <simon@simonhugh.xyz>
2021-12-10 18:02:15 +0000

    add[qutebrowser]: set default zoom to 150

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -12,3 +12,4 @@ c.tabs.show                 = 'never'
 c.tabs.tabs_are_windows     = True
 c.url.default_page          = 'search.simonhugh.xyz'
 c.url.start_pages           = ['search.simonhugh.xyz']
+c.zoom.default  =  150
