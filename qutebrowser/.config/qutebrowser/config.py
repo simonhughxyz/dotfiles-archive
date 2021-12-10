dbb325c007c03d2264700e86e3e139dbfba6f66a
Simon H Moore <simon@simonhugh.xyz>
2021-12-10 18:01:53 +0000

    add[qutebrowser]: default and startpage

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -10,3 +10,5 @@ c.confirm_quit              = ['downloads']
 c.content.fullscreen.window = True
 c.tabs.show                 = 'never'
 c.tabs.tabs_are_windows     = True
+c.url.default_page          = 'search.simonhugh.xyz'
+c.url.start_pages           = ['search.simonhugh.xyz']
