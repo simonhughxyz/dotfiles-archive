892c490668754f29c9327e2dc98889f57fcd58a5
Simon H Moore <simon@simonhugh.xyz>
2021-12-10 18:02:44 +0000

    add[qutebrowser]: set english and german spellcheck

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -8,6 +8,7 @@ config.load_autoconfig(False)
 # ==================== General Settings ==================================
 c.confirm_quit              = ['downloads']
 c.content.fullscreen.window = True
+c.spellcheck.languages      = ["de-DE", "en-GB", "en-US"]
 c.tabs.show                 = 'never'
 c.tabs.tabs_are_windows     = True
 c.url.default_page          = 'search.simonhugh.xyz'
