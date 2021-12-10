779db535149017e87c02a1172b514f4c0e5c55c9
Simon H Moore <simon@simonhugh.xyz>
2021-12-10 18:00:45 +0000

    add[qutebrowser]: initial config

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,12 @@
+# 
+# Qutebrowser Config
+# 
+
+# don't load auto autoconfig.yml
+config.load_autoconfig(False)
+
+# ==================== General Settings ==================================
+c.confirm_quit              = ['downloads']
+c.content.fullscreen.window = True
+c.tabs.show                 = 'never'
+c.tabs.tabs_are_windows     = True
