8745ee7b7e9c3bce86661627576ee59ebefd97c0
Simon H Moore <simon@simonhugh.xyz>
2021-12-11 01:33:10 +0000

    add[qutebrowser]: user both hosts and adblocking

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -23,6 +23,7 @@ c.content.cookies.accept    = 'no-3rdparty'
 c.content.javascript.can_access_clipboard  = False
 c.content.headers.referer   = 'same-domain'
 c.content.blocking.enabled  = True
+c.content.blocking.method   = 'both'
 
 # ==================== Aliases ===========================================
 c.aliases = {
