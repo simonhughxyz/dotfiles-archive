be42a16b5ab32166c1fb63d6b206aec6fe1784e0
Simon H Moore <simon@simonhugh.xyz>
2021-12-12 01:51:43 +0000

    add[qutebrowser]: don't send do not track

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -25,6 +25,7 @@ c.content.cookies.accept    = 'no-3rdparty'
 c.content.javascript.can_access_clipboard   = False
 c.content.headers.referer   = 'same-domain'
 c.content.dns_prefetch      = False
+c.content.headers.do_not_track = False    # can be used to fingerprint
 c.content.blocking.enabled  = True
 c.content.blocking.method   = 'both'
 c.content.blocking.hosts.block_subdomains   = True
