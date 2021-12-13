e54d5442a98d16fc15307d9c736f70890fcc4d18
Simon H Moore <simon@simonhugh.xyz>
2021-12-13 00:22:03 +0000

    add[qutebrowser]: set dark mode background threshold to 150

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -55,6 +55,7 @@ c.content.blocking.adblock.lists    = [
 c.colors.webpage.darkmode.enabled  =  True
 c.colors.webpage.darkmode.algorithm  =  'lightness-cielab'
 c.colors.webpage.darkmode.policy.images  =  'smart'
+c.colors.webpage.darkmode.threshold.background  =  150
 
 # ==================== Aliases ===========================================
 c.aliases = {
