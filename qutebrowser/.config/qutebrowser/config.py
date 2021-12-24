c0f5b8ddac250298c96c88e685f13f4dae8eebf8
Simon H Moore <simon@simonhugh.xyz>
2021-12-24 00:20:11 +0000

    refactor[qutebrowser]: remove old load_autoconfig

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -2,9 +2,6 @@
 # Qutebrowser Config
 # 
 
-# don't load auto autoconfig.yml
-config.load_autoconfig(False)
-
 # ==================== General Settings ==================================
 c.confirm_quit              = ['downloads']
 c.content.fullscreen.window = True
