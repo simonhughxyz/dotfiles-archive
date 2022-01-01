91f73392d7bfc96c806b9b765006e9175234ac6d
Simon H Moore <simon@simonhugh.xyz>
2022-01-01 01:00:51 +0000

    add[qutebrowser]: open new instance in new window

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -8,6 +8,7 @@ c.content.fullscreen.window = True
 c.spellcheck.languages      = ["de-DE", "en-GB", "en-US"]
 c.tabs.show                 = 'never'
 c.tabs.tabs_are_windows     = True
+c.new_instance_open_target  = 'window'
 c.url.default_page          = 'about:blank'
 c.url.start_pages           = ['about:blank']
 c.zoom.default              = 150
